0222a8f8dede7403a06a4dafe397dcc62a50aeca
Jason Mwaura Mbugua <80914491+JASON-d-tech@users.noreply.github.com>
2023-02-11 16:44:16 +0300

    ProfileScreen.js

---
+++ ProfileScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View,TourchableOpacity,SafeAreaView } from "react-native";
+import{Text,TextInput} from "react-native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+
+
